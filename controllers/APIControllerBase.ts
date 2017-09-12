1172953a0c816d4fbe88292c3784c1b0107ca420
pelle <pelle@flork.dk>
2017-09-12 14:06:14 +0200

    Added skeleton for workout controller

---
+++ controllers/APIControllerBase.ts
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+export class APIControllerBase {
+    protected SetHeaders(res): void {
+        res.setHeader('Content-Type', 'application/json');
+    }
+
+    protected CheckContentType(req): boolean {
+        return req.get('Content-Type') == 'application/json';
+    }
+
+    protected SendWrongContentTypeError(res): void {
+        this.SetHeaders(res);
+        res.status(500);
+        res.send(JSON.stringify({ err: 'Content type not JSON' }));
+    }
+
+    protected CheckData(req): boolean {
+        return typeof req.body.data == 'string';
+    }
+
+    protected SendWrongDataError(res): void {
+        res.status(500);
+        res.send(JSON.stringify({ err: 'Sent data is incorrect' }));
+    }
+
+    protected SendNotFoundError(res): void {
+        res.status(404);
+        res.send(JSON.stringify({ err: 'Not found' }));
+    }
+}
--- controllers/WorkoutProgram.ts
+++ controllers/WorkoutProgram.ts
@@ -2,21 +2,12 @@ import { APIControllerBase } from './APIControllerBase';
 var express = require('express');
 var router = express.Router();
 
-export class ExampleController extends APIControllerBase {
-    exampleData: any[] = [
-        { id: 1, data: "Example Data" },
-        { id: 2, data: "John Hitler" },
-        { id: 3, data: "Sausage Party" },
-        { id: 4, data: "hurh durh" },
-        { id: 5, data: "bob the builder" },
-        { id: 6, data: "numse leg" },
-        { id: 7, data: "JOHN CENA, DUDHU DUH UDUHU" },
-        { id: 8, data: "Kartoffel" },
-    ];
-
+export class WorkoutController extends APIControllerBase  {
     public Get(req, res): void {
         this.SetHeaders(res);
         let id = req.params['id'];
+        throw new Error("Not implemented");
+        /*
         let foundData = this.exampleData.find((val) => val.id == id);
         if (foundData) {
             res.send(JSON.stringify(foundData));
@@ -24,18 +15,24 @@ export class ExampleController extends APIControllerBase {
         else {
             this.SendNotFoundError(res);
         }
+        */
     }
 
     public Post(req, res): void {
         this.SetHeaders(res);
+        throw new Error("Not implemented");
+        /*
         let id = this.exampleData[this.exampleData.length - 1].id + 1;
         let index = this.exampleData.push({ id: id, data: "Example" });
         res.location(req.get('host') + req.baseUrl + '/' + id);
         res.status(200);
         res.send(JSON.stringify(this.exampleData[index]));
+        */
     }
 
     public Put(req, res): void {
+        throw new Error("Not implemented");
+        /*
         // Guards against wrong content types
         if (!this.CheckContentType(req)) {
             this.SendWrongContentTypeError(res);
@@ -60,9 +57,12 @@ export class ExampleController extends APIControllerBase {
         else {
             this.SendNotFoundError(res);
         }
+        */
     }
 
     public Patch(req, res): void {
+        throw new Error("Not implemented");
+        /*
         // Guards against wrong content types
         if (!this.CheckContentType(req)) {
             this.SendWrongContentTypeError(res);
@@ -81,12 +81,14 @@ export class ExampleController extends APIControllerBase {
         else {
             this.SendNotFoundError(res);
         }
+        */
     }
 
     public Delete(req, res): void {
         this.SetHeaders(res);
         let id = req.params['id'];
-
+        throw new Error("Not implemented");
+        /*
         let i = -1;
         let foundData = this.exampleData.find((val, index) => {
             i = index;
@@ -100,26 +102,27 @@ export class ExampleController extends APIControllerBase {
         else {
             this.SendNotFoundError(res);
         }
+        */
     }
 }
 
-let Controller = new ExampleController();
-let ExampleControllerRoutes = router;
+let Controller = new WorkoutController();
+let WorkoutControllerRoutes = router;
 
-ExampleControllerRoutes.get('/:id', (req, res) => {
+WorkoutControllerRoutes.get('/:id', (req, res) => {
     Controller.Get(req, res);
 });
-ExampleControllerRoutes.post('/', (req, res) => {
+WorkoutControllerRoutes.post('/', (req, res) => {
     Controller.Post(req, res);
 });
-ExampleControllerRoutes.put('/:id', (req, res) => {
+WorkoutControllerRoutes.put('/:id', (req, res) => {
     Controller.Put(req, res);
 });
-ExampleControllerRoutes.patch('/:id', (req, res) => {
+WorkoutControllerRoutes.patch('/:id', (req, res) => {
     Controller.Patch(req, res);
 });
-ExampleControllerRoutes.delete('/:id', (req, res) => {
+WorkoutControllerRoutes.delete('/:id', (req, res) => {
     Controller.Delete(req, res);
 });
 
-export { ExampleControllerRoutes };
+export { WorkoutControllerRoutes };
