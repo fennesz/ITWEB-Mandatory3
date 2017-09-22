f7e6e6ce53f4de9baa188a320ca5b5b2c95a40fd
mdc <201205975@post.au.dk>
2017-09-22 10:16:14 +0200

    Removed app_server from the directories

--- app_server/routes.ts
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-import { WorkoutControllerRoutes } from './controllers/WorkoutProgramController';
-
-/* Example web api controller*/
-router.use('/api/workoutprogram', WorkoutControllerRoutes);
-
-export = router;
---
+++ src/AppRouterModule/AppRouterModule.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRouterModuleComponent } from './AppRouterModule.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AppRouterModuleComponent]
+})
+export class AppRouterModuleModule { }
