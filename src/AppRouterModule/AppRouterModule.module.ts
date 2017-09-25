0c48cd398102c484912cb639a332b77aee24f7d0
Fennesz <martin@fedthund.dk>
2017-09-25 16:46:25 +0200

    Added missing slash

--- src/AppRouterModule/AppRouterModule.module.ts
+++ src/AppRouterModule/AppRouterModule.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: 'workoutprogram', component: WorkoutProgramListComponent },
+  { path: '/workoutprogram', component: WorkoutProgramListComponent },
   { path: '',   redirectTo: '/workoutprogram', pathMatch: 'full' },
   { path: '404', component: NotfoundComponent},
   { path: '**', redirectTo: '/404', pathMatch: 'full' }
